ea3b3681f1286330a1a51bf4b67f0bc46b6a6301
Matthew Chipkin <mchipkin21@gmail.com>
2025-10-30 21:12:10 -0500

    Added create method/action to Products Controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,23 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+    render template: "products/index"
+  end
+
   def show
     @product = Product.find(params["id"])
     render template: "products/show"
   end
 
-  def index
-    @products = Product.all
-    render template: "products/index"
+  def create
+    @product = Product.create(
+      name: "Test Post Name",
+      price: 10,
+      image_url: "test-post-image.jpeg",
+      description: "Test Description"
+    )
+    render template: "products/show"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
 
   get "/products/:id" => "products#show"
   get "/products" => "products#index"
+  post "/products" => "products#create"
 end

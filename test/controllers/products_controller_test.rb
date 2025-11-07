234e869e85990aba85d26fc3bf682f994f36fe40
Matthew Chipkin <mchipkin21@gmail.com>
2025-11-06 21:24:28 -0600

    added some tests for create action in controller

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create" do
+
+    assert_difference "Product.count", 1 do
+      post "/products.json", params: { name: "Matthew", price: 4.00, image_url: "matt.jpg", description: "Actualize", inventory: 5}
+      assert_response 201
+    end
+
+    assert_difference "Product.count", 0 do
+      post "/products.json", params: {}
+      assert_response 422
+    end
+  end
 end
